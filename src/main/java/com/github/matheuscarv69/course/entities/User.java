ec4e64a5c26b5a6359eefe6410a5e6b7a680f394
matheuscarv69 <matheus9126@gmail.com>
2021-01-12 20:37:50 -0400

    H2 database, test profile, JPA

--- src/main/java/com/github/matheuscarv69/course/entities/User.java
+++ src/main/java/com/github/matheuscarv69/course/entities/User.java
@@ -5,6 +5,10 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
@@ -12,15 +16,17 @@ import java.util.Objects;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
+@Entity
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private String phone;
     private String password;
-
 
     @Override
     public boolean equals(Object o) {
